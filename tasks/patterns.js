7d64c4ee530f966b6844d4083c89954f272f2cd7
Brian Mathews <spoonart@gmail.com>
2014-12-31 12:27:25 -0800

    Removes pattern task comment

--- tasks/patterns.js
+++ tasks/patterns.js
@@ -106,7 +106,6 @@ module.exports = function(grunt) {
                     nav[category][subCategory] = nav[category][subCategory] || [];
                     nav[category][subCategory].push(contentMap[filePath]);
                 }
-                // console.log(category, subCategory);
             });
         });
 
